b4073662251dfa5c851e27a19fd11d996dba0182
dumaya <alexis.dumay@axa.fr>
2019-10-11 11:13:38 +0200

    First commit 1

---
+++ src/main/java/dumaya/dev/BibWeb/BibWebApplication.java
@@ -0,0 +1,13 @@
+package dumaya.dev.BibWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dumaya/dev/BibWeb/BibWebApplicationTests.java
@@ -0,0 +1,16 @@
+package dumaya.dev.BibWeb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BibWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
